d5441edcbd9f06fa7c4a7c486aa0b1ac2c23dbfe
Dinu2226 <ddrazvan22@gmail.com>
2023-01-09 16:14:09 +0200

    Added findPlaces option for hospitals

--- AngularDemoArcGISFirebase-main/src/app/pages/esri-map/esri-map.component.ts
+++ AngularDemoArcGISFirebase-main/src/app/pages/esri-map/esri-map.component.ts
@@ -39,6 +39,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   _FeatureLayer;
   _Graphic;
   _GraphicsLayer;
+  _Locator;
   _Route;
   _RouteParameters;
   _FeatureSet;
@@ -53,7 +54,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
 
   // Attributes
   zoom = 10;
-  center: Array<number> = [26.1521, 44.4396];
+  center: Array<number> = [26.10300, 44.43325];
   basemap = "streets-vector";
   loaded = false;
   pointCoords: number[] = [26.1521, 44.4396];
@@ -78,7 +79,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
       setDefaultOptions({ css: true });
 
       // Load the modules for the ArcGIS API for JavaScript
-      const [esriConfig, Map, MapView, FeatureLayer, Graphic, Point, GraphicsLayer, route, RouteParameters, FeatureSet] = await loadModules([
+      const [esriConfig, Map, MapView, FeatureLayer, Graphic, Point, GraphicsLayer, locator,  route, RouteParameters, FeatureSet] = await loadModules([
         "esri/config",
         "esri/Map",
         "esri/views/MapView",
@@ -86,6 +87,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
         "esri/Graphic",
         "esri/geometry/Point",
         "esri/layers/GraphicsLayer",
+        "esri/rest/locator",
         "esri/rest/route",
         "esri/rest/support/RouteParameters",
         "esri/rest/support/FeatureSet"
@@ -98,6 +100,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
       this._FeatureLayer = FeatureLayer;
       this._Graphic = Graphic;
       this._GraphicsLayer = GraphicsLayer;
+      this._Locator = locator;
       this._Route = route;
       this._RouteParameters = RouteParameters;
       this._FeatureSet = FeatureSet;
@@ -113,7 +116,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
       this.addFeatureLayers();
       this.addGraphicLayers();
 
-      this.addPoint(this.pointCoords[1], this.pointCoords[0], true);
+      // this.addPoint(this.pointCoords[1], this.pointCoords[0], true);
 
 
       // Initialize the MapView
@@ -135,11 +138,63 @@ export class EsriMapComponent implements OnInit, OnDestroy {
 
       await this.view.when(); // wait for map to load
       console.log("ArcGIS map loaded");
+
+      this.view.when(()=>{
+        this.findPlaces(this.view.center);
+      });
+      // this.addRouter();
+
       console.log("Map center: " + this.view.center.latitude + ", " + this.view.center.longitude);
       return this.view;
     } catch (error) {
       console.log("EsriLoader: ", error);
     }
+  }
+
+  findPlaces(pt) {
+    const geocodingServiceUrl = "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+ 
+    const params = {
+      categories: ["medical clinic", "hospital"],
+      location: pt, 
+      outFields: ["PlaceName","Place_addr"]
+    }
+    this._Locator.addressToLocations(geocodingServiceUrl, params).then((results)=> {
+      this.showResults(results);
+    });
+ 
+  }
+
+  showResults(results) {
+    this.view.popup.close();
+      this.view.graphics.removeAll();
+      results.forEach((result)=>{
+        this.view.graphics.add(
+          new this._Graphic({
+            attributes: result.attributes,
+            geometry: result.location,
+            symbol: {
+              type: "simple-marker",
+              color: "orange",
+              size: "10px",
+              outline: {
+                color: "#ffffff",
+                width: "2px"
+              }
+            },
+            popupTemplate: {
+              title: "{PlaceName}",
+              content: "{Place_addr}" + "<br><br>" + result.location.x.toFixed(5) + "," + result.location.y.toFixed(5)
+            }
+          }));
+      });
+      if (results.length) {
+        const g = this.view.graphics.getItemAt(0);
+        this.view.popup.open({
+          features: [g],
+          location: g.geometry
+        });
+      }
   }
 
   addGraphicLayers() {
@@ -210,39 +265,9 @@ export class EsriMapComponent implements OnInit, OnDestroy {
     this.timeoutHandler = setTimeout(() => {
       // code to execute continuously until the view is closed
       // ...
-      this.animatePointDemo();
+      // this.animatePointDemo();
       this.runTimer();
     }, 200);
-  }
-
-  animatePointDemo() {
-    this.removePoint();
-    switch (this.dir) {
-      case 0:
-        this.pointCoords[1] += 0.01;
-        break;
-      case 1:
-        this.pointCoords[0] += 0.02;
-        break;
-      case 2:
-        this.pointCoords[1] -= 0.01;
-        break;
-      case 3:
-        this.pointCoords[0] -= 0.02;
-        break;
-    }
-
-    this.count += 1;
-    if (this.count >= 10) {
-      this.count = 0;
-      this.dir += 1;
-      if (this.dir > 3) {
-        this.dir = 0;
-      }
-    }
-
-    this.addPoint(this.pointCoords[1], this.pointCoords[0], true);
-    this.fbs.syncPointItem(this.pointCoords[1], this.pointCoords[0]);
   }
 
   stopTimer() {
